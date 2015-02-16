327148b3c125ab8b70ecba8d7f756b0a4adc0385
Stefan Seifert <nine@detonation.org>
2015-02-16 20:38:02 +0100

    Fix installation via panda

--- Build.pm
+++ Build.pm
@@ -5,12 +5,29 @@ use Shell::Command;
 use LibraryMake;
 
 class Build is Panda::Builder {
+    sub get_config_var(Str $name) {
+        return chomp(qqx/python -c "import distutils.sysconfig; print(distutils.sysconfig.get_config_var('$name'));"/);
+    }
+
     method build($dir) {
         my Str $blib = "$dir/blib";
         rm_f("$dir/lib/Inline/pyhelper.so");
         rm_rf($blib);
         mkpath("$blib/lib/Inline");
-        make($dir, "$blib/lib");
+
+        my %vars = get-vars("$blib/lib");
+        %vars<INCLUDEPY> = get_config_var('INCLUDEPY');
+        my $library = get_config_var('LIBRARY');
+        $library ~~ s/\.a$//;
+        $library ~~ s/^lib//;
+        %vars<LIBRARYPY> = $library;
+        %vars<LIBPLPY> = get_config_var('LIBPL');
+        process-makefile($dir, %vars);
+
+        my $goback = $*CWD;
+        chdir($dir);
+        shell(%vars<MAKE>);
+        chdir($goback);
     }
 }
 
